387698cb099dfcabff0c2c073923a7918819d056
muskaplan <muskaplan@innova.com.tr>
2022-10-02 15:59:51 +0300

    spring boot - elasticsearch

---
+++ spring-elasticsearch/src/main/java/com/mustafakaplan/springelasticsearch/SpringElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.mustafakaplan.springelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringElasticsearchApplication.class, args);
+    }
+
+}
---
+++ spring-elasticsearch/src/test/java/com/mustafakaplan/springelasticsearch/SpringElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafakaplan.springelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElasticsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
