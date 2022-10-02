5c3d7546919452023eb984c0ef9bf3c1c5c9c4fd
muskaplan <muskaplan@innova.com.tr>
2022-10-02 20:10:04 +0300

    spring boot - postgresql

---
+++ spring-datajpa-postgresql/src/main/java/com/mustafakaplan/springdatajpapostgresql/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.mustafakaplan.springdatajpapostgresql;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringDatajpaPostgresqlApplication.class);
+    }
+
+}
---
+++ spring-datajpa-postgresql/src/main/java/com/mustafakaplan/springdatajpapostgresql/SpringDatajpaPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.mustafakaplan.springdatajpapostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatajpaPostgresqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDatajpaPostgresqlApplication.class, args);
+    }
+
+}
