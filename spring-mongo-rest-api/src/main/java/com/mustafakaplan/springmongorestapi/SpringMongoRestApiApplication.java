6b837cfceb3f989c352e76fdb0bf40e3240e2dd9
muskaplan <muskaplan@innova.com.tr>
2022-10-02 14:54:30 +0300

    refactor spring boot project

---
+++ spring-mongo-rest-api/src/main/java/com/mustafakaplan/springmongorestapi/SpringMongoRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.mustafakaplan.springmongorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMongoRestApiApplication.class, args);
+    }
+
+}
---
+++ spring-mongo-rest-api/src/test/java/com/mustafakaplan/springmongorestapi/SpringMongoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafakaplan.springmongorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
